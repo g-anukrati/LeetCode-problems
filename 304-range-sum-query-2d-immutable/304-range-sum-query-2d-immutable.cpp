2d3005d8d2dd07425b50f93b13c199dbc5927f90
Anukrati Gupta <74946698+g-anukrati@users.noreply.github.com>
2022-06-04 01:08:35 +0530

    Time: 616 ms (47.61%), Space: 148.1 MB (48.14%) - LeetHub

---
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
@@ -0,0 +1,29 @@
+class NumMatrix {
+public:
+    vector<vector<int>>mat;
+    NumMatrix(vector<vector<int>>& matrix) {
+        mat = matrix;
+        for(int i = 0; i<mat.size(); i++){
+            int sum = 0;
+            for(int j = 0; j<mat[i].size(); j++){
+                sum+=mat[i][j];
+                mat[i][j] = sum;
+            }
+        }
+    }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        int sum = 0;
+        for(int i = row1; i<=row2;i++){
+            if(col1 == 0){
+                sum+=mat[i][col2];
+            }
+            else{
+                sum+=mat[i][col2];
+                sum-=mat[i][col1-1];
+            }
+        }
+        return sum;
+        
+    }
+};
