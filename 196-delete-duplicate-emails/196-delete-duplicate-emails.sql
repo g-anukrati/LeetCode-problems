a11cac9164730957558206cdc8d2d241da910b1f
Anukrati Gupta <74946698+g-anukrati@users.noreply.github.com>
2022-07-03 17:10:12 +0530

    Time: 1868 ms (5.02%), Space: 0B (100.00%) - LeetHub

--- 196-delete-duplicate-emails/196-delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/196-delete-duplicate-emails.sql
@@ -2,14 +2,21 @@
 # Write your MySQL query statement below
 
 #Using Join:
+delete p2 
+from person as p1 join person as p2 
+on p1.email=p2.email 
+and p1.id<p2.id;
+
 
 
 #Without Using Join (Correlated Query):
+/*
+delete p from Person p
+where id != (select min(id) from Person
+           where email = p.email)
+*/
 
-#delete p from Person p
-#where id != (select min(id) from Person
-#            where email = p.email)
-
+/*
 with temp_table as(
 select id, email,
 row_number() over (partition by email order by id asc) as r_num 
@@ -17,3 +24,4 @@ from person
 )
 delete from person where id in(
 select id from temp_table where r_num <> 1)
+*/
