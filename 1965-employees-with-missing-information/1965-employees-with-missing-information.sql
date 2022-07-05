461764ccfc32cd53e6904b47a1db7b2ace2a3ca0
Anukrati Gupta <74946698+g-anukrati@users.noreply.github.com>
2022-07-05 13:48:46 +0530

    Time: 455 ms (88.42%), Space: 0B (100.00%) - LeetHub

---
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT T.employee_id
+FROM  
+  (SELECT * FROM Employees LEFT JOIN Salaries USING(employee_id)
+   UNION 
+   SELECT * FROM Employees RIGHT JOIN Salaries USING(employee_id))
+AS T
+WHERE T.salary IS NULL OR T.name IS NULL
+ORDER BY employee_id;
+
+#full join is not allowed in mysql
+#using instead of on beacuse col name same hai 
