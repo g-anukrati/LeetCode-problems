b1a8a0795c68f21612a394bb7ba8d7707e20b839
Anukrati Gupta <74946698+g-anukrati@users.noreply.github.com>
2022-07-09 13:54:14 +0530

    Time: 211 ms (8.99%), Space: 53.6 MB (19.98%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> map;
+        for(auto i:nums){
+            map[i]++;
+        }
+        for(auto i:map){
+            if(i.second  > 1){
+                return true;
+            }
+        }
+        return false;
+    }
+};
